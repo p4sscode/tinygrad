bedb9fe23ebdee27ea711d5b650e097ee1302276
p4sscode <p4ssenger.developer@gmail.com>
2024-09-20 09:12:52 -0300

    hotfix: change if for elig

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -319,8 +319,8 @@ class Kernel:
         except KernelOptError: continue
         for opt in tc.opt_seq:
           if opt == "UR": [self.apply_opt(Opt(OptOps.UNROLL,tc_opts.axes[2]-self.first_reduce,amt), append_opt=False) for _,amt in tc.reduce_axes]
-          if opt == "UP": [self.apply_opt(Opt(OptOps.UPCAST,tc_opts.axes[tc_dim],amt), append_opt=False) for tc_dim,amt in tc.early_upcast_axes]
-          if opt == "LC": [self.apply_opt(Opt(OptOps.LOCAL,tc_opts.axes[tc_dim],amt), append_opt=False) for tc_dim,amt in tc.threads]
+          elif opt == "UP": [self.apply_opt(Opt(OptOps.UPCAST,tc_opts.axes[tc_dim],amt), append_opt=False) for tc_dim,amt in tc.early_upcast_axes]
+          elif opt == "LC": [self.apply_opt(Opt(OptOps.LOCAL,tc_opts.axes[tc_dim],amt), append_opt=False) for tc_dim,amt in tc.threads]
         self.tensor_core = tc
         self.use_tensor_cores = use_tensor_cores  # TC=2 will do the shape ops without the WMMA
         return True
