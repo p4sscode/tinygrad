d9d46223ab5b6acc71a3863516f237c8d3b98973
p4sscode <p4ssenger.developer@gmail.com>
2024-09-26 16:19:50 -0300

    base case

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -50,8 +50,20 @@ base_rewrite = PatternMatcher([
   # alu/gep
   (UPat(UOps.ALU, arg=BinaryOps.SHL, name="op"), lambda r,op: f"({r[op.src[0]]}<<{r[op.src[1]]})"),
   (UPat(UOps.ALU, arg=BinaryOps.SHR, name="op"), lambda r,op: f"({r[op.src[0]]}>>{r[op.src[1]]})"),
-  (UPat(UOps.ALU, arg=BinaryOps.ADD, name="op"), lambda r,op: f"({r[op.src[0]]}+{r[op.src[1]]})"),
-  (UPat(UOps.ALU, arg=BinaryOps.MUL, name="op"), lambda r,op: f"({r[op.src[0]]}*{r[op.src[1]]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.ADD, src=(UPat.var("a"), UPat.var("b")), name="op"),
+   lambda r,op,a,b: f"({strip_parens(r[a]) if a.arg == op.arg else r[a]}+{strip_parens(r[b]) if b.arg == op.arg else r[b]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.SUB, name="op"), lambda r,op: f"({r[op.src[0]]}-{r[op.src[1]]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.IDIV, name="op"), lambda r,op: f"({r[op.src[0]]}/{r[op.src[1]]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.MUL, src=(UPat.var("a"), UPat.var("b")), name="op"),
+   lambda r,op,a,b: f"({strip_parens(r[a]) if a.arg == op.arg else r[a]}*{strip_parens(r[b]) if b.arg == op.arg else r[b]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.MOD, name="op"), lambda r,op: f"({r[op.src[0]]}%{r[op.src[1]]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.CMPLT, name="op"), lambda r,op: f"({r[op.src[0]]}<{r[op.src[1]]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.CMPLT, name="op"), lambda r,op: f"({r[op.src[0]]}!={r[op.src[1]]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.XOR, src=(UPat.var("a"), UPat.var("b")), name="op"),
+   lambda r,op,a,b: f"({strip_parens(r[a]) if a.arg == op.arg else r[a]}^{strip_parens(r[b]) if b.arg == op.arg else r[b]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.AND, name="op"), lambda r,op: f"({r[op.src[0]]}&{r[op.src[1]]})"),
+  (UPat(UOps.ALU, arg=BinaryOps.OR, name="op"), lambda r,op: f"({r[op.src[0]]}|{r[op.src[1]]})"),
+
   (UPat(UOps.ALU, name="x"), lambda r,x: r.code_for_op[x.arg](
     *([strip_parens(r[v]) if v.arg == x.arg and x.arg in {BinaryOps.ADD, BinaryOps.MUL, BinaryOps.XOR} else r[v] for v in x.src]), x.dtype)),
   (UPat(UOps.GEP, name="x"), lambda r,x: r[x.src[0]] + \
@@ -92,11 +104,7 @@ class CStyleLanguage(Renderer):
     UnaryOps.RECIP: lambda x,dtype: f"(1/{x})",
     UnaryOps.NEG: lambda x,dtype: f"-{x}",
     UnaryOps.EXP2: lambda x,dtype: f"exp2({x})", UnaryOps.LOG2: lambda x,dtype: f"log2({x})", UnaryOps.SIN: lambda x,dtype: f"sin({x})",
-    BinaryOps.SHL: lambda a,b,dtype: f"({a}<<{b})", BinaryOps.SHR: lambda a,b,dtype: f"({a}>>{b})",
-    BinaryOps.ADD: lambda a,b,dtype: f"({a}+{b})", BinaryOps.SUB: lambda a,b,dtype: f"({a}-{b})", BinaryOps.MAX: lambda a,b,dtype: f"max({a},{b})",
-    BinaryOps.IDIV: lambda a,b,dtype: f"({a}/{b})", BinaryOps.MUL: lambda a,b,dtype: f"({a}*{b})", BinaryOps.MOD: lambda a,b,dtype: f"({a}%{b})",
-    BinaryOps.CMPLT: lambda a,b,dtype: f"({a}<{b})", BinaryOps.CMPNE: lambda a,b,dtype: f"({a}!={b})", BinaryOps.XOR: lambda a,b,dtype: f"({a}^{b})",
-    BinaryOps.AND: lambda a,b,dtype: f"({a}&{b})", BinaryOps.OR: lambda a,b,dtype: f"({a}|{b})",
+    BinaryOps.MAX: lambda a,b,dtype: f"max({a},{b})",
     TernaryOps.WHERE: lambda a,b,c,dtype: f"({a}?{b}:{c})"}
 
   string_rewrite = base_rewrite
