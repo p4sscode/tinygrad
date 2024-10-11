b3f4a12e0bbb3a441b9f85df521f8c2d23c40106
p4sscode <p4ssenger.developer@gmail.com>
2024-10-11 10:46:01 -0300

    hotfix: fold duplicate casting

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -399,6 +399,8 @@ class AMDRenderer(CStyleLanguage):
     (BinaryOps.MAX,None): lambda a,b:f"__ocml_fmax_f32({a},{b})"}
 
   extra_matcher = PatternMatcher([
+    # this rule changes behaviour as it may avoid precision loss without intermediate bfloat16 casting
+    (UPat(UOps.CAST, dtype=dtypes.float, name='c', src=(UPat(UOps.CAST, dtype=dtypes.bfloat16, src=(UPat(name='var'),)),)), lambda c,var: var.cast(c.dtype)), # noqa:E501
     (UPat(UOps.CAST, dtype=dtypes.float, name='c', src=(UPat(UOps.CAST, dtype=dtypes.bfloat16, src=(UPat(name='var'),)),)), lambda c,var: var.cast(c.dtype)), # noqa:E501
     (UPat(UOps.ALU, arg=TernaryOps.WHERE, dtype=dtypes.bfloat16, src=(UPat(name="c"), UPat(name="x", dtype=dtypes.bfloat16), UPat(name="y", dtype=dtypes.bfloat16))), # noqa:E501
      lambda c,x,y: (UOp(UOps.ALU, arg=TernaryOps.WHERE, dtype=dtypes.float, src=(c,x.cast(dtypes.float),y.cast(dtypes.float))).cast(dtypes.bfloat16))), # noqa:E501
