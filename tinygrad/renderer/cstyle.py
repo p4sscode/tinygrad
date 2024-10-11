060ae5e7078965945e89b9675ae77f1af1a28c33
p4sscode <p4ssenger.developer@gmail.com>
2024-10-11 10:55:26 -0300

    hotfix: avoid duplicate cast

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -399,8 +399,8 @@ class AMDRenderer(CStyleLanguage):
     (BinaryOps.MAX,None): lambda a,b:f"__ocml_fmax_f32({a},{b})"}
 
   extra_matcher = PatternMatcher([
+    (UPat(UOps.CAST, name='c', src=(UPat(UOps.CAST, name="c2", src=(UPat.var('var'),)),)), lambda c,c2,var: var.cast(c.dtype) if c.dtype==c2.dtype else c), # noqa:E501
     # this rule changes behaviour as it may avoid precision loss without intermediate bfloat16 casting
-    (UPat(UOps.CAST, dtype=dtypes.float, name='c', src=(UPat(UOps.CAST, dtype=dtypes.bfloat16, src=(UPat(name='var'),)),)), lambda c,var: var.cast(c.dtype)), # noqa:E501
     (UPat(UOps.CAST, dtype=dtypes.float, name='c', src=(UPat(UOps.CAST, dtype=dtypes.bfloat16, src=(UPat(name='var'),)),)), lambda c,var: var.cast(c.dtype)), # noqa:E501
     (UPat(UOps.ALU, arg=TernaryOps.WHERE, dtype=dtypes.bfloat16, src=(UPat(name="c"), UPat(name="x", dtype=dtypes.bfloat16), UPat(name="y", dtype=dtypes.bfloat16))), # noqa:E501
      lambda c,x,y: (UOp(UOps.ALU, arg=TernaryOps.WHERE, dtype=dtypes.float, src=(c,x.cast(dtypes.float),y.cast(dtypes.float))).cast(dtypes.bfloat16))), # noqa:E501
