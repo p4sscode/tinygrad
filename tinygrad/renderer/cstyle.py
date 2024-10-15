9bfe7470c412efca168480f312dcc1abfad01bd6
p4sscode <p4ssenger.developer@gmail.com>
2024-10-15 14:52:41 -0300

    hotfix: add intermediary cast

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -453,8 +453,13 @@ class AMDRenderer(CStyleLanguage):
       lambda b,x,y: UOp(UOps.ALU, arg=TernaryOps.WHERE, dtype=dtypes.float, src=(b,x.cast(dtypes.float),y.cast(dtypes.float))).cast(dtypes.bfloat16)),
     (UPat(UOps.ALU, dtype=dtypes.bfloat16, name="x"),
       lambda x: (UOp(x.op, dtypes.float, tuple(vv.cast(dtypes.float) for vv in x.src), x.arg).cast(dtypes.bfloat16))),
+    # (UPat(UOps.CAST, name="x",  src=(UPat.any(dtype=dtypes.bfloat16),)), lambda x: None if,
+    (
+      UPat(UOps.CAST, name="x", src=(UPat.var("y", dtype=dtypes.bfloat16),)),
+      lambda x, y: None if x.dtype == dtypes.float else y.cast(dtypes.float).cast(x.dtype),
+     ),
 
-    (UPat(UOps.CAST, dtype=dtypes.float, src=(UPat.var("x",dtype=dtypes.bfloat16),)), lambda x: (x.bitcast(dtypes.uint)<<16).bitcast(dtypes.float)),
+    (UPat(UOps.CAST, dtype=dtypes.float, src=(UPat.var("x", dtype=dtypes.bfloat16),)), lambda x: (x.bitcast(dtypes.uint)<<16).bitcast(dtypes.float)),
 
     # (UPat(UOps.CAST, dtype=dtypes.float, src=(UPat.var("x", dtype=dtypes.bfloat16),)),
     #   lambda x: UOp(UOps.ALU, arg=BinaryOps.SHL, dtype=dtypes.float, src=(x.cast(dtypes.ushort), UOp.const(dtypes.int, 16)))
