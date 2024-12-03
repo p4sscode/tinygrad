9dadf70c02622f851f62d7029fc6cbe76861c118
p4sscode <p4ssenger.developer@gmail.com>
2024-12-03 14:03:39 -0300

    unify amd opts_seq with other backends

--- tinygrad/renderer/__init__.py
+++ tinygrad/renderer/__init__.py
@@ -19,7 +19,6 @@ class TensorCore: # D = A * B + C, A is (M x K), B is (K x N), C and D are (M x 
   st1_pattern: Optional[Tuple[Tuple[Tuple[int,int], ...], Tuple[Tuple[int,int], ...]]] = None # pattern to fix shapetracker for A
   st2_pattern: Optional[Tuple[Tuple[Tuple[int,int], ...], Tuple[Tuple[int,int], ...]]] = None # pattern to fix shapetracker for B
   expanded_shape: Optional[Tuple[int, ...]] = None
-  opts_seq: Tuple[str,str] = ("UP","LC") # upcast input, local the thread pattern
   def __str__(self): return "_".join(["WMMA"] + list(map(str, self.dims)) + [self.dtype_in.name, self.dtype_out.name])
 
 @dataclass
