87a035cf1accafb29bb82a25bc1feb041b2808b6
ignaciosica <mignacio.sica@gmail.com>
2024-12-04 06:35:33 -0800

    hotfix: mypy

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -631,7 +631,7 @@ class Kernel:
 
           srcs = list((ret.src[0] if ret.src[0].op is not Ops.CAST else ret.src[0].src[0]).src)
           for i, tc_pattern in enumerate([tc.st1_pattern, tc.st2_pattern]):
-            if tc_pattern: srcs[i] = srcs[i].view(get_tc_swizzle_st((srcs[i] if srcs[i].op is Ops.LOAD else srcs[i].src[0]).st.shape, *tc_pattern))
+            if tc_pattern: srcs[i] = srcs[i].view(get_tc_swizzle_st((srcs[i] if srcs[i].op is Ops.LOAD else srcs[i].src[0]).st_arg.shape,*tc_pattern))
 
             if self.use_tensor_cores == 3:  # for TC=3, emulate the warp addressing with locals
               local_shape = tuple(1 if i >= self.first_reduce and i < self.first_upcast else s for i, s in enumerate(self.full_shape))
