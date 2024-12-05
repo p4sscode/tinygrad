0b1a1a6a8f4a86f2df0b578cd5e8ac86c10cc001
ignaciosica <mignacio.sica@gmail.com>
2024-12-04 18:56:59 -0800

    add f64 tc

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -298,20 +298,31 @@ class CUDARenderer(CStyleLanguage):
   local_max = (1024, 1024, 64)
   shared_max = 49152
   # https://docs.nvidia.com/cuda/parallel-thread-execution/#warp-level-matrix-fragment-mma-16816-float
-  tensor_cores_81616 = [TensorCore(dims=(8,16,16), st1_pattern=(((1,1),(1,2),(1,5),(0,2),(0,3)),((1,0),(0,4),(1,3),(0,0),(0,1),(1,4))),
-      threads=[(0,2),(0,2),(1,2),(1,2),(1,2)], st2_pattern=(((1,1),(1,2),(1,4),(0,0),(0,1)),((0,2),(0,3),(0,4),(1,0),(1,3),(1,5))),
-      reduce_axes=[(0,2),(1,2),(2,2),(3,2)], st3_pattern=(((0,0),(0,1),(1,5),(0,2),(0,3)),((1,0),(1,1),(1,2),(1,3),(1,4),(0,4))),
-      upcast_axes=([(2,2),(1,2),(0,2)],[(4,2),(3,2)],[(5,2),(4,2)]), dtype_in=di, dtype_out=do)
+  # pat=(((0,0),(0,1),(0,2),(0,3),(0,4)),((1,0),(1,1),(1,2),(1,3),(1,4),(1,5))),
+  tensor_cores_81616 = [TensorCore(dims=(8,16,16),st1_pattern=(((1,1),(1,2),(1,5),(0,2),(0,3)),((1,0),(0,4),(1,3),(0,0),(0,1),(1,4))),
+      threads=[(0,2),(0,2),(1,2),(1,2),(1,2)],st2_pattern=(((1,1),(1,2),(1,4),(0,0),(0,1)),((0,2),(0,3),(0,4),(1,0),(1,3),(1,5))),
+      reduce_axes=[(0,2),(1,2),(2,2),(3,2)],st3_pattern=(((0,0),(0,1),(1,5),(0,2),(0,3)),((1,0),(1,1),(1,2),(1,3),(1,4),(0,4))),
+      upcast_axes=([(2,2),(1,2),(0,2)],[(4,2),(3,2)],[(5,2),(4,2)]),dtype_in=di,dtype_out=do)
     for di,do in ([(dtypes.half,dtypes.float),(dtypes.bfloat16,dtypes.float)])]
-  tensor_cores_tf32 = [TensorCore(dims=(8, 16, 8), st1_pattern=(((1,0),(1,1),(1,4),(0,2),(0,3)),((0,0),(1,3),(0,1),(1,2),(0,4))),
-      threads=[(0, 2), (0, 2), (1, 2), (1, 2), (1, 2)], st2_pattern=(((1,0),(1,1),(1,3),(0,0),(0,1)),((0,2),(0,3),(0,4),(1,4),(1,2))),
-      upcast_axes=[[(3, 2), (4, 2)], [(4, 2)], [(4, 2), (3, 2)]], st3_pattern=(((0,0),(0,1),(1,4),(0,2),(0,3)),((1,0),(1,1),(1,2),(1,3),(0,4))),
-      reduce_axes=[(0, 2), (1, 2), (2, 2)], dtype_in=dtypes.float, dtype_out=dtypes.float)]
-  # tensor_cores_f64 = [TensorCore(dims=(8, 16, 8), st1_pattern=(((1,0),(1,1),(1,4),(0,2),(0,3)),((0,0),(1,3),(0,1),(1,2),(0,4))),
-  #     threads=[(0, 2), (0, 2), (1, 2), (1, 2), (1, 2)], st2_pattern=(((1,0),(1,1),(1,3),(0,0),(0,1)),((0,2),(0,3),(0,4),(1,4),(1,2))),
-  #     upcast_axes=[[(3, 2), (4, 2)], [(4, 2)], [(4, 2), (3, 2)]], st3_pattern=(((0,0),(0,1),(1,4),(0,2),(0,3)),((1,0),(1,1),(1,2),(1,3),(0,4))),
-  #     reduce_axes=[(0, 2), (1, 2), (2, 2)], dtype_in=dtypes.float64, dtype_out=dtypes.float64)]
-  tensor_cores_80 = tensor_cores_81616 + tensor_cores_tf32 # + tensor_cores_f64
+  tensor_cores_tf32 = [TensorCore(dims=(8,16,8),st1_pattern=(((1,0),(1,1),(1,4),(0,2),(0,3)),((0,0),(1,3),(0,1),(1,2),(0,4))),
+      threads=[(0,2),(0,2),(1,2),(1,2),(1,2)],st2_pattern=(((1,0),(1,1),(1,3),(0,0),(0,1)),((0,2),(0,3),(0,4),(1,4),(1,2))),
+      upcast_axes=[[(3,2),(4,2)],[(4,2)],[(4,2),(3,2)]],st3_pattern=(((0,0),(0,1),(1,4),(0,2),(0,3)),((1,0),(1,1),(1,2),(1,3),(0,4))),
+      reduce_axes=[(0,2),(1,2),(2,2)],dtype_in=dtypes.float,dtype_out=dtypes.float)]
+  # slower than non tc alternative?
+  tensor_cores_f64 = [TensorCore(dims=(8,8,4),threads=[(0,2),(0,2),(1,2),(1,2),(1,2)],
+      st1_pattern=(((1,0),(1,1),(0,2),(0,3),(0,4)),((0,0),(0,1),(1,2))),reduce_axes=[(0,2),(1,2)],upcast_axes=[[(0,1)],[(0,1)],[(2,2)]],
+      st2_pattern=(((1,0),(1,1),(1,2),(0,0),(0,1)),((0,2),(0,3),(0,4))),dtype_in=dtypes.float64,dtype_out=dtypes.float64)]
+  # tensor_cores_s8 = [
+  #   TensorCore(
+  #     dims=(8, 16, 32),
+  #     threads=[(0, 2), (0, 2), (1, 2), (1, 2), (1, 2)],
+  #     reduce_axes=[(0, 2), (1, 2), (2, 2), (3, 2), (4, 2)],
+  #     upcast_axes=[[(5,8)],[(5,4)],[(5,8)]],
+  #     dtype_in=dtypes.int8,
+  #     dtype_out=dtypes.int32,
+  #   )
+  # ]
+  tensor_cores_80 = tensor_cores_81616 + tensor_cores_tf32 + tensor_cores_f64 + tensor_cores_s8
   def __init__(self, arch:str): self.tensor_cores, self.arch = CUDARenderer.tensor_cores_80 if int(arch[3:]) >= 80 else [], arch
   def __reduce__(self): return self.__class__, (self.arch,)
 
@@ -350,16 +361,16 @@ class CUDARenderer(CStyleLanguage):
     for name, (N, M, K), dtype_in, dtype_out, _, _, upcast_axes, _ in dedup([uop.arg for uop in uops if uop.op is Ops.WMMA]):
       upcast_sizes = [prod(size for _, size in upcast) for upcast in upcast_axes]
       wmma_dtypes = [self.render_dtype(dtype.vec(size)) for dtype, size in zip([dtype_in, dtype_in, dtype_out], upcast_sizes)]
-      n_operands = [size*dtype.itemsize//4 for dtype, size in zip([dtype_in, dtype_in, dtype_out], upcast_sizes)] # 4 => CUDA reg size in bytes
+      n_operands = [size * dtype.itemsize // dtype_out.itemsize for dtype, size in zip([dtype_in, dtype_in, dtype_out], upcast_sizes)]
       operands = [f"%{i}" for i in range(sum(n_operands))]
 
       # mma operands => {c}, {a}, {b}, {c}
       prefix.append(f"""__device__ {wmma_dtypes[2]} __{name}({wmma_dtypes[0]} a, {wmma_dtypes[1]} b, {wmma_dtypes[2]} c){{
-  int *a_pk = (int *)(&a), *b_pk = (int *)(&b);\n  asm("mma.sync.aligned.m{M}n{N}k{K}.row.col.{dt_map_out[dtype_out]}.{dt_map_in[dtype_in]}.{dt_map_in[dtype_in]}.{dt_map_out[dtype_out]}"
+  double *a_pk = (double *)(&a), *b_pk = (double *)(&b);\n  asm("mma.sync.aligned.m{M}n{N}k{K}.row.col.{dt_map_out[dtype_out]}.{dt_map_in[dtype_in]}.{dt_map_in[dtype_in]}.{dt_map_out[dtype_out]}"
       "{{{", ".join(operands[:n_operands[2]])}}}, {{{", ".join(operands[n_operands[2]:n_operands[2]+n_operands[0]])}}},"
       "{{{", ".join(operands[-n_operands[1]:])}}}, {{{", ".join(operands[:n_operands[2]])}}};"
-    : {", ".join([f'"+f"(c.{_nms[i]})' for i in range(n_operands[2])])}
-    : {", ".join([f'"r"(a_pk[{i}])' for i in range(n_operands[0])])}, {", ".join([f'"r"(b_pk[{i}])' for i in range(n_operands[1])])});
+    : {", ".join([f'"+d"(c.{_nms[i]})' for i in range(n_operands[2])])}
+    : {", ".join([f'"d"(a_pk[{i}])' for i in range(n_operands[0])])}, {", ".join([f'"d"(b_pk[{i}])' for i in range(n_operands[1])])});
   return c;\n}}""") # noqa:E501
 
     return super().render_kernel(function_name, kernel, bufs, uops, prefix=prefix)
