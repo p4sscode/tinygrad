0d5d67048023018d6dd3e6e5ddfdf6f443afdca1
p4sscode <p4ssenger.developer@gmail.com>
2024-12-26 12:12:13 -0300

    amx working

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -175,7 +175,8 @@ class ClangRenderer(CStyleLanguage):
     CStyleLanguage.extra_matcher
 
   if AMX: tensor_cores = [TensorCore(dims=(sz,sz,1), threads=1, upcast_size=(sz,sz,sz*sz), dtype_in=dt, dtype_out=dt,
-                                     swizzle=(None, ((),(4,5,6,7,0,1,2,3)), None)) for dt,sz in [(dt, 64 // dt.itemsize) for dt in [dtypes.float]]]
+            swizzle=(None, ((),(4,5,6,7,0,1,2,3)), None), opts=(("u",0),("u",0),("u",0),("u",0),("u",1),("u",1),("u",1),("u",1)))
+            for dt,sz in [(dt, 64 // dt.itemsize) for dt in [dtypes.float]]]
 
   def render_vector_prefix(self, dt:DType) -> str:
     return f"typedef {self.render_dtype(dt.scalar())} {self.render_dtype(dt)} __attribute__((aligned({(sz:=dt.itemsize)}),vector_size({sz})));"
