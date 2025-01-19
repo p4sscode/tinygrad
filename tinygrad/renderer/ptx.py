c6b9ae7fb64be7c90ef8d27abc8e878a73cab4d9
ignaciosica <mignacio.sica@gmail.com>
2025-01-18 18:07:16 -0800

    more cleaning

--- tinygrad/renderer/ptx.py
+++ tinygrad/renderer/ptx.py
@@ -60,21 +60,20 @@ def render_wmma(ctx: "PTXRenderer", x: UOp):
   assert ctx.wmma_r, "registry values for wmma must be populated"
   _, (N, M, K), dt_in, dt_out, _, _, _, _ = x.arg
   dt_map_in, dt_map_out = {dtypes.float: "tf32", dtypes.half: "f16"}, {dtypes.float: "f32", dtypes.half: "f16"}
-  elems_in, elems_out = 4 // dt_in.itemsize, 4 // dt_out.itemsize
 
   # pack input and acc
   for src, regs in enumerate(ctx.wmma_r[:3]):
     for i in range(len(regs)):
-      elems = elems_in if src < 2 else elems_out
+      elems = 4 // (dt_in if src < 2 else dt_out).itemsize # packed elements per register
       yield f"mov.b32 {regs[i]}, " + (f"{{{', '.join(ctx.r[x.src[src]][i*elems : (i+1)*elems])}}}" if elems > 1 else ctx.r[x.src[src]][i]) + ";"
 
   yield (f'mma.sync.aligned.m{M}n{N}k{K}.row.col.{dt_map_out[dt_out]}.{dt_map_in[dt_in]}.{dt_map_in[dt_in]}.{dt_map_out[dt_out]}{" " * 12}'
-       + f'{{{", ".join(ctx.wmma_r[3])}}}, {{{", ".join(ctx.wmma_r[0])}}}, '
-       + f'{{{", ".join(ctx.wmma_r[1])}}}, {{{", ".join(ctx.wmma_r[2])}}};')
+       + f'{{{", ".join(ctx.wmma_r[3])}}}, {{{", ".join(ctx.wmma_r[0])}}}, {{{", ".join(ctx.wmma_r[1])}}}, {{{", ".join(ctx.wmma_r[2])}}};')
 
   # unpack output
   for i in range(len(ctx.wmma_r[3])):
-    yield "mov.b32 "+ (f"{{{', '.join(ctx.r[x][i*elems_out : (i+1)*elems_out])}}}" if elems_out > 1 else ctx.r[x][i]) + f", {ctx.wmma_r[3][i]};"
+    elems = 4 // dt_out.itemsize
+    yield "mov.b32 "+ (f"{{{', '.join(ctx.r[x][i*elems : (i+1)*elems])}}}" if elems > 1 else ctx.r[x][i]) + f", {ctx.wmma_r[3][i]};"
 
 def modifier(a: DType, b: DType): return '.rzi' if dtypes.is_int(a) and dtypes.is_float(b) else '.rn' if dtypes.is_float(a) and \
   (a.itemsize < b.itemsize or dtypes.is_int(b) or b == dtypes.bool) else ''
@@ -191,8 +190,8 @@ class PTXRenderer(Renderer):
       elif u.op is Ops.DEFINE_GLOBAL: bufs.append((f"data{u.arg}", u.dtype))
       elif u.op is Ops.WMMA:
         # register for packing/unpacking output, input and acc
-        self.wmma_r = [[ssa("wmma_ina", dtype="b32") for _ in range(0, len(r[u.src[0]]), 4 // u.arg[2].itemsize)],
-                       [ssa("wmma_inb", dtype="b32") for _ in range(0, len(r[u.src[1]]), 4 // u.arg[2].itemsize)],
+        self.wmma_r = [[ssa("wmma_in", dtype="b32") for _ in range(0, len(r[u.src[0]]), 4 // u.arg[2].itemsize)],
+                       [ssa("wmma_in", dtype="b32") for _ in range(0, len(r[u.src[1]]), 4 // u.arg[2].itemsize)],
                        [ssa("wmma_acc", dtype="b32") for _ in range(0, len(r[u.src[2]]), 4 // u.arg[3].itemsize)],
                        [ssa("wmma_out", dtype="b32") for _ in range(0, u.dtype.itemsize // 4)]]
         r[u] = [ssa("wmma", dtype=self.types[u.dtype.scalar()]) for _ in range(u.dtype.count)]
